00b10f07cb6227bd3cb7ce6082f5f647fcee60a8
Stephen Fink <stephenfink8@gmail.com>
2020-08-08 18:39:59 -0500

    changed code to having array of strings rather than a Math.floor

--- Develop/script.js
+++ Develop/script.js
@@ -4,19 +4,18 @@ var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 //WHEN I click the button to generate a password
 // Assignment Code
-var caseUpper = String.fromCharCode(Math.floor(Math.random()* 26)+97)
-var caseLower = String.fromCharCode(Math.floor(Math.random()*26)+97)
-var caseNum = String.fromCharCode(Math.floor(Math.random()* 10)+48)
-var symbolLetters = Math.floor(Math.random() * 12)
-      for(i = 0; i <= symbolLetters.length; i++){}
+var caseUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var caseLower = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var caseNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-var symbolLetters = "!@#$%^&*(){}"
+
+var symbolLetters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-"]
 var userInput = ""
-var  passLength 
+var passLength
 var password
-var symbolCase 
+var symbolCase
 var lengthPass
-var PWChoice = ""
+var PWChoice = []
 // Write password to the #password input
 // Assignment Code
 
@@ -25,92 +24,71 @@ var PWChoice = ""
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
 
 }
 function generatePassword() {
   passWordLength();
-  passAskUpper();
-  getRandomLower();
-  getRandomNumber();
-  getRandomSymbols();
-  
-  
-  
+  var upperCase = confirm("do you want upper case? yes or no");
+  if (upperCase === true) {
+    [PWChoice] = [PWChoice] + caseUpper
+  }
+  else { [PWChoice] = [PWChoice] }
+
+
+  var lowerCase = confirm("do you want lower case? yes or no");
+  if (lowerCase === true) {
+    [PWChoice] = [PWChoice] + caseLower
+  }
+  else {
+    [PWChoice] = [PWChoice]
+  };
+
+
+  var numCase = confirm("do you want numbers? yes or no");
+  if (numCase === true) {
+    [PWChoice] = [PWChoice] + [caseNum]
+  }
+  else {
+    [PWChoice] = [PWChoice]
+  };
+
+
+  var symbolCase = confirm("do you want symbols? yes or no");
+  if (symbolCase === true) {
+    [PWChoice] = [PWChoice] + symbolCase
+  }
+  else { [PWChoice] + [PWChoice] }
+
+
+
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-  function passWordLength() {
-    while(true){
+function passWordLength() {
+  while (true) {
     var passLength = prompt("how long you want the password between 8 and 128");
-    if (passLength == null){
+    if (passLength == null) {
       alert("Exiting Password Generator")
-    }else{
-    if((passLength <8) || (passLength > 128)){
-      alert("you need at least 8 or less than 128 characters")}
-    
-    else{return parseInt(passLength)}
+    } else {
+      if ((passLength < 8) || (passLength > 128)) {
+        alert("you need at least 8 or less than 128 characters")
+      }
+      else { return passLength}
     };
-  };
-  }
-  function passAskUpper() {
-    while(true){
-    var upperCase = prompt("do you want upper case? yes or no");
-    if (upperCase == null){
-      PWChoice = PWChoice
-    }else{
-    if(upperCase === yes){
-      PWChoice = PWChoice + caseUpper
-    }
-    else { PWChoice = PWChoice}
-  }
-  }
-  }
-  function passAskLower() {
-    while(true){
-    var lowerCase = prompt("do you want lower case? yes or no");
-    if(lowerCase == null){
-      PWChoice = PWChoice
-    }else{
-    if(lowerCase === yes){
-      PWChoice = PWChoice + caseLower
-    }
-    else{
-      PWChoice = PWChoice
-    };
-  };
   };
 }
-  function passAskNum() {
-    while(true){
-    var numCase = prompt("do you want numbers? yes or no");
-    if(numCase == null){
-      PWChoice = PWChoice
-    }else{
-    if(numCase === yes){
-      PWChoice = PWChoice + caseNum
-    }
-    else{
-      PWChoice = PWChoice
-    };
-  };
-  };
+
+  
+
+
+function buildPassFun(caseUpper, caseLower, caseNum, symbolLetters){
+ var password = ""
+ for(var i = 1; i <= passLength; i++){
+   var randomPass = confirmPrompt[Math.floor(Math.random()* confirmPrompt.length)]
+ }
+
 }
-  function passAskSym(params) {
-    while(true){
-    var symbolCase = prompt("do you want symbols? yes or no");
-    if(symbolCase == null){
-      PWChoice = PWChoice
-    }else{
-    if(symbolCase === yes){
-      PWChoice += PWChoice
-    }
-    else{PWChoice + PWChoice}
-    };
-  };
-  }
-
-
 

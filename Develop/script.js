dab61c182e788a4ef6a8863b3196209fe0261921
Stephen Fink <stephenfink8@gmail.com>
2020-08-08 15:49:36 -0500

    added prompts for each question going to be used

--- Develop/script.js
+++ Develop/script.js
@@ -16,10 +16,20 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;}
 
+    //when we click the button it it should
+    //prompt for how long of password
+    prompt("how long you want the password between 8 and 128")
+    //if i want ...
+    // upper case
+    prompt("do you want upper case? yes or no")
+    //lower case
+    prompt("do you want lower case? yes or no")
+    //numbers
+    prompt("do you want numbers? yes or no")
+    //symbols
+    prompt("do you want symbols? yes or no")
+    //then it will use the length of password to fill in with the cases i used
 
-  function lengthSize(){
-    
-  }
 
 //prompts set up
 generateBtn.addEventListener("click", writePassword);

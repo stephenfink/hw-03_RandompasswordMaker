e84aeb27137f23e4faa75d84d5a9ab24f22693c6
Stephen Fink <stephenfink8@gmail.com>
2020-08-08 19:17:09 -0500

    moved code and move into spots to work with overall function

--- Develop/script.js
+++ Develop/script.js
@@ -3,19 +3,18 @@ var generateBtn = document.querySelector("#generate");
 //possible content arrays I can make each letter/number into a unit
 generateBtn.addEventListener("click", writePassword);
 //WHEN I click the button to generate a password
+
 // Assignment Code
-var caseUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var caseLower = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var caseUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var caseLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var caseNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
-var symbolLetters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-"]
-var userInput = ""
-var passLength
-var password
-var symbolCase
-var lengthPass
-var PWChoice = []
+var symbolLetters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-"];
+var password;
+var passLength;
+var lengthPass = [];
+var PWChoice = [];
 // Write password to the #password input
 // Assignment Code
 
@@ -29,66 +28,58 @@ function writePassword() {
 
 }
 function generatePassword() {
-  passWordLength();
-  var upperCase = confirm("do you want upper case? yes or no");
-  if (upperCase === true) {
-    [PWChoice] = [PWChoice] + caseUpper
-  }
-  else { [PWChoice] = [PWChoice] }
-
-
-  var lowerCase = confirm("do you want lower case? yes or no");
-  if (lowerCase === true) {
-    [PWChoice] = [PWChoice] + caseLower
-  }
-  else {
-    [PWChoice] = [PWChoice]
-  };
-
-
-  var numCase = confirm("do you want numbers? yes or no");
-  if (numCase === true) {
-    [PWChoice] = [PWChoice] + [caseNum]
-  }
-  else {
-    [PWChoice] = [PWChoice]
-  };
-
-
-  var symbolCase = confirm("do you want symbols? yes or no");
-  if (symbolCase === true) {
-    [PWChoice] = [PWChoice] + symbolCase
-  }
-  else { [PWChoice] + [PWChoice] }
-
-
-
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-function passWordLength() {
-  while (true) {
-    var passLength = prompt("how long you want the password between 8 and 128");
-    if (passLength == null) {
-      alert("Exiting Password Generator")
-    } else {
-      if ((passLength < 8) || (passLength > 128)) {
-        alert("you need at least 8 or less than 128 characters")
-      }
-      else { return passLength}
-    };
-  };
-}
-
   
+  passLength = (prompt("how long you want the password between 8 and 128"));
+  if ((passLength => 8 && passLength <=128)) {
+  }
+  else {alert("must color between the lines")
+return};
 
 
-function buildPassFun(caseUpper, caseLower, caseNum, symbolLetters){
- var password = ""
- for(var i = 1; i <= passLength; i++){
-   var randomPass = confirmPrompt[Math.floor(Math.random()* confirmPrompt.length)]
- }
 
+upperCase = confirm("do you want upper case? yes or no");
+if (upperCase === true) {
+  PWChoice = PWChoice + caseUpper
 }
+else {PWChoice = PWChoice}
 
+
+lowerCase = confirm("do you want lower case? yes or no");
+if (lowerCase === true) {
+  PWChoice = PWChoice + caseLower
+}
+else {
+  PWChoice = PWChoice
+};
+
+
+numCase = confirm("do you want numbers? yes or no");
+if (numCase === true) {
+  PWChoice = PWChoice + caseNum
+}
+else {
+  PWChoice = PWChoice
+};
+
+
+var symbolCase = confirm("do you want symbols? yes or no");
+if (symbolCase === true) {
+  PWChoice = PWChoice + symbolCase
+}
+else {PWChoice + PWChoice}
+
+
+
+
+// Add event listener to generate button
+
+
+
+  var password = ""
+  for (var i = 1; i <= lengthPass; i++) {
+    var RNGen = PWChoice[Math.floor(Math.random() * PWChoice.length)]
+    password = RNGen + password;
+  }
+  return password;
+  
+}

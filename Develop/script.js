6e284977b62a16e146d5478763f52157825af6dc
Stephen Fink <stephenfink8@gmail.com>
2020-08-08 19:46:19 -0500

    using consolelog to see errors, also correct syntex of code

--- Develop/script.js
+++ Develop/script.js
@@ -5,16 +5,14 @@ generateBtn.addEventListener("click", writePassword);
 //WHEN I click the button to generate a password
 
 // Assignment Code
-var caseUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var caseLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var caseNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-
-var symbolLetters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-"];
+var caseUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
+var caseLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
+var caseNum = ["0","1","2","3","4","5","6","7","8","9",]
+var symbolLetters = ["!","@","#","$","%","^","&","*","(",")","+","-",];
 var password;
 var passLength;
 var lengthPass = [];
-var PWChoice = [];
+var pWChoice = [];
 // Write password to the #password input
 // Assignment Code
 
@@ -31,6 +29,7 @@ function generatePassword() {
   
   passLength = (prompt("how long you want the password between 8 and 128"));
   if ((passLength => 8 && passLength <=128)) {
+    console.log(passLength)
   }
   else {alert("must color between the lines")
 return};
@@ -39,47 +38,60 @@ return};
 
 upperCase = confirm("do you want upper case? yes or no");
 if (upperCase === true) {
-  PWChoice = PWChoice + caseUpper
+  pWChoice = pWChoice + caseUpper
+  console.log(upperCase)
 }
-else {PWChoice = PWChoice}
-
+else {pWChoice = pWChoice
+  console.log(upperCase);}
 
 lowerCase = confirm("do you want lower case? yes or no");
 if (lowerCase === true) {
-  PWChoice = PWChoice + caseLower
+  pWChoice = pWChoice + caseLower
+  console.log(lowerCase);
 }
 else {
-  PWChoice = PWChoice
+  pWChoice = pWChoice
+  console.log(lowerCase);
 };
 
 
 numCase = confirm("do you want numbers? yes or no");
 if (numCase === true) {
-  PWChoice = PWChoice + caseNum
+  pWChoice = pWChoice + caseNum
+  console.log(numCase);
 }
 else {
-  PWChoice = PWChoice
+  pWChoice = pWChoice
+  console.log(numCase);
 };
 
 
 var symbolCase = confirm("do you want symbols? yes or no");
 if (symbolCase === true) {
-  PWChoice = PWChoice + symbolCase
+  pWChoice = pWChoice + symbolLetters
+  console.log(symbolCase);
+  console.log(pWChoice)
 }
-else {PWChoice + PWChoice}
-
-
+else {pWChoice + pWChoice
+  console.log(symbolCase);
+  console.log(pWChoice);
+}
 
 
 // Add event listener to generate button
 
 
-
-  var password = ""
-  for (var i = 1; i <= lengthPass; i++) {
-    var RNGen = PWChoice[Math.floor(Math.random() * PWChoice.length)]
-    password = RNGen + password;
+  var password ="";
+ 
+  for (var i = 0; i < lengthPass.length; i++) {
+    var rNGen = pWChoice[Math.floor(Math.random()*passLength.length)];
+    password = rNGen + password;
+    console.log(rNGen)
   }
-  return password;
+  for(var i = 1; i <lengthPass.value; i++){
+    var rNG = pWChoice[Math.random(Math.random()*passLength.value)];
+    password = rNG + password;
+    console.log(rNG)
+  }
   
 }
